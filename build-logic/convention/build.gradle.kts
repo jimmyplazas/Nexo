7197305b446398b065a0237995780ce89e0ca3e5
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-25 17:03:12 -0500

    feat: Add `BuildKonfig`, `Room`, and other convention plugins

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,6 +11,9 @@ dependencies {
     compileOnly(libs.android.tools.common)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.gradlePlugin)
+    compileOnly(libs.androidx.room.gradle.plugin)
+    implementation(libs.buildkonfig.gradlePlugin)
+    implementation(libs.buildkonfig.compiler)
 }
 
 java {
@@ -36,6 +39,34 @@ gradlePlugin {
         register("androidApplication") {
             id = "dev.alejo.convention.android.application"
             implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidComposeApplication") {
+            id = "dev.alejo.convention.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("cmpApplication") {
+            id = "dev.alejo.convention.cmp.application"
+            implementationClass = "CmpApplicationConventionPlugin"
+        }
+        register("kmpApplication") {
+            id = "dev.alejo.convention.kmp.library"
+            implementationClass = "KmpLibraryConventionPlugin"
+        }
+        register("cmpLibrary") {
+            id = "dev.alejo.convention.cmp.library"
+            implementationClass = "CmpLibraryConventionPlugin"
+        }
+        register("cmpFeature") {
+            id = "dev.alejo.convention.cmp.feature"
+            implementationClass = "CmpFeatureConventionPlugin"
+        }
+        register("buildKonfig") {
+            id = "dev.alejo.convention.buildkonfig"
+            implementationClass = "BuildKonfigConventionPlugin"
+        }
+        register("roomConventionPlugin") {
+            id = "dev.alejo.convention.room"
+            implementationClass = "RoomConventionPlugin"
         }
     }
 }
