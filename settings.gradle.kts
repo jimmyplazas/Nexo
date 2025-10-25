1fb6e4c9ddbcadd36e8de0c744cfca9f6c56135c
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-24 22:14:21 -0500

    feat: Move Android application config to convention plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "Nexo"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             mavenContent {
