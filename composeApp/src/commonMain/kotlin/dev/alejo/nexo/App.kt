7134c58384f05c33a2d88831b2125dfa2a4f5b08
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-27 13:21:00 -0500

    feat: Use `NexoTheme` in App composable

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import dev.alejo.core.designsystem.theme.NexoTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,7 +23,7 @@ import nexo.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    NexoTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
