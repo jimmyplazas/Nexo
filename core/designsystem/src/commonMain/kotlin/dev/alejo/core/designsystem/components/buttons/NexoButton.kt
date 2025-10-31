a0a4d1eed4893f3a5849bc7fcdfbf050a7a6df95
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-31 00:26:58 -0500

    feat: Implement `NexoButton` component with styles and loading state

--- core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/buttons/NexoButton.kt
+++ core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/buttons/NexoButton.kt
@@ -1,11 +1,24 @@
 package dev.alejo.core.designsystem.components.buttons
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.designsystem.theme.extended
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 enum class NexoButtonStyle {
@@ -22,17 +35,104 @@ fun NexoButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     style: NexoButtonStyle = NexoButtonStyle.PRIMARY,
-    isEnabled: Boolean = false,
+    isEnabled: Boolean = true,
     isLoading: Boolean = false,
-    leadingIcon: @Composable () -> Unit = {}
+    leadingIcon: @Composable (() -> Unit)? = {}
 ) {
+    val colors = when(style) {
+        NexoButtonStyle.PRIMARY -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+            disabledContainerColor = MaterialTheme.colorScheme.extended.disabledFill,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        NexoButtonStyle.DESTRUCTIVE_PRIMARY -> ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.error,
+            contentColor = MaterialTheme.colorScheme.onError,
+            disabledContainerColor = MaterialTheme.colorScheme.extended.disabledFill,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        NexoButtonStyle.SECONDARY -> ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.extended.textSecondary,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        NexoButtonStyle.DESTRUCTIVE_SECONDARY -> ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.error,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+        NexoButtonStyle.TEXT -> ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.tertiary,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = MaterialTheme.colorScheme.extended.textDisabled
+        )
+    }
+
+    val defaultBorderStroke = BorderStroke(
+        width = 1.dp,
+        color = MaterialTheme.colorScheme.extended.disabledOutline
+    )
+
+    val border = when {
+        style == NexoButtonStyle.PRIMARY && !isEnabled -> defaultBorderStroke
+        style == NexoButtonStyle.SECONDARY && !isEnabled -> defaultBorderStroke
+        style == NexoButtonStyle.DESTRUCTIVE_PRIMARY && !isEnabled -> defaultBorderStroke
+        style == NexoButtonStyle.DESTRUCTIVE_SECONDARY -> {
+            val borderColor = if(isEnabled) {
+                MaterialTheme.colorScheme.extended.destructiveSecondaryOutline
+            } else {
+                MaterialTheme.colorScheme.extended.disabledOutline
+            }
+            BorderStroke(
+                width = 1.dp,
+                color = borderColor
+            )
+        }
+        else -> null
+    }
+
     Button(
         onClick = onClick,
         modifier = modifier,
         enabled = isEnabled,
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(8.dp),
+        colors = colors,
+        border = border
     ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(15.dp)
+                    .alpha(if (isLoading) 1f else 0f),
+                strokeWidth = 1.5.dp,
+                color = Color.Black
+            )
 
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(
+                    8.dp,
+                    Alignment.CenterHorizontally
+                ),
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.alpha(
+                    if (isLoading) 0f else 1f
+                )
+            ) {
+                leadingIcon?.let {
+                    it.invoke()
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
+            }
+        }
     }
 }
 
