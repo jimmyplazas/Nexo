7087e7b24ddb95a060fff3510616aeea2852fefb
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-31 10:43:19 -0500

    feat: Add `NexoTextField` composable

---
+++ core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/textfields/NexoTextField.kt
@@ -0,0 +1,207 @@
+package dev.alejo.core.designsystem.components.textfields
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NexoTextField(
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    title: String? = null,
+    supportingText: String? = null,
+    isError: Boolean = false,
+    singleLine: Boolean = false,
+    enabled: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    onFocusChanged: (Boolean) -> Unit = {},
+) {
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    LaunchedEffect(isFocused) {
+        onFocusChanged(isFocused)
+    }
+
+    Column(
+        modifier = modifier
+    ) {
+        if (title != null) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.extended.textSecondary
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+        BasicTextField(
+            state = state,
+            enabled = enabled,
+            lineLimits = if (singleLine) {
+                TextFieldLineLimits.SingleLine
+            } else {
+                TextFieldLineLimits.Default
+            },
+            textStyle = MaterialTheme.typography.bodyMedium.copy(
+                color = if (enabled) {
+                    MaterialTheme.colorScheme.onSurface
+                } else {
+                    MaterialTheme.colorScheme.extended.textPlaceholder
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType
+            ),
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
+            interactionSource = interactionSource,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = when {
+                        isFocused -> MaterialTheme.colorScheme.primary.copy(
+                            alpha = 0.05f
+                        )
+
+                        enabled -> MaterialTheme.colorScheme.surface
+                        else -> MaterialTheme.colorScheme.extended.secondaryFill
+                    },
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .border(
+                    width = 1.dp,
+                    color = when {
+                        isFocused -> MaterialTheme.colorScheme.primary
+                        isError -> MaterialTheme.colorScheme.error
+                        else -> MaterialTheme.colorScheme.outline
+                    },
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(12.dp),
+            decorator = { innerBox ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    if (state.text.isEmpty() && placeholder != null) {
+                        Text(
+                            text = placeholder,
+                            color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
+                    }
+                }
+            }
+        )
+
+        if (supportingText != null) {
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = supportingText,
+                color = if (isError) {
+                    MaterialTheme.colorScheme.error
+                } else {
+                    MaterialTheme.colorScheme.extended.textTertiary
+                },
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NexoTextFieldEmptyPreview() {
+    NexoTheme {
+        NexoTextField(
+            state = rememberTextFieldState(),
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "test@gamil.com",
+            title = "Email",
+            supportingText = "This is a supporting text",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NexoTextFieldFilledPreview() {
+    NexoTheme {
+        NexoTextField(
+            state = rememberTextFieldState(
+                initialText = "test@gamil.com"
+            ),
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "test@gamil.com",
+            title = "Email",
+            supportingText = "This is a supporting text",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NexoTextFieldDisabledPreview() {
+    NexoTheme {
+        NexoTextField(
+            state = rememberTextFieldState(),
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "test@gamil.com",
+            title = "Email",
+            enabled = false,
+            supportingText = "This is a supporting text",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NexoTextFieldErrorPreview() {
+    NexoTheme {
+        NexoTextField(
+            state = rememberTextFieldState(),
+            modifier = Modifier
+                .width(300.dp),
+            placeholder = "test@gamil.com",
+            title = "Email",
+            enabled = false,
+            isError = true,
+            supportingText = "This is not a valid email",
+        )
+    }
+}
