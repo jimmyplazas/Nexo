54c33fdb81cab2d0f10fbf0ce3b2522f6755a011
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-31 00:26:43 -0500

    feat: Add `NexoIconButton` component

---
+++ core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/buttons/NexoIconButton.kt
@@ -0,0 +1,71 @@
+package dev.alejo.core.designsystem.components.buttons
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NexoIconButton(
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    OutlinedIconButton(
+        onClick = onClick,
+        modifier = modifier
+            .size(45.dp),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(
+            width = 1.dp,
+            color = MaterialTheme.colorScheme.outline
+        ),
+        colors = IconButtonDefaults.outlinedIconButtonColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.extended.textSecondary
+        )
+    ) {
+        content()
+    }
+}
+
+@Preview
+@Composable
+private fun NexoIconButtonPreview() {
+    NexoTheme {
+        NexoIconButton(
+            modifier = Modifier
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun NexoIconButtonDarkThemePreview() {
+    NexoTheme(darkTheme = true) {
+        NexoIconButton(
+            modifier = Modifier
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = null
+            )
+        }
+    }
+}
