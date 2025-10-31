8684b707a2e06f82fa9e78a64376af5b762c6ce4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-31 14:48:43 -0500

    feat: Make NexoSurface scrollable and add horizontal padding

--- core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/layout/NexoSurface.kt
+++ core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/layout/NexoSurface.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -47,6 +49,8 @@ fun NexoSurface(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState())
                 ) {
                     content()
                 }
