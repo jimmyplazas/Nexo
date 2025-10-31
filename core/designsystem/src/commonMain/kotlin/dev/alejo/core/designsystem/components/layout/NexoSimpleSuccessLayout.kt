651475fdecc640d9cefc1c02e3eb05746efc29bf
Jimmyale3102 <jimmyale3102@gmail.com>
2025-10-31 16:32:00 -0500

    feat: Add `NexoSimpleSuccessLayout` component

---
+++ core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/layout/NexoSimpleSuccessLayout.kt
@@ -0,0 +1,97 @@
+package dev.alejo.core.designsystem.components.layout
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import dev.alejo.core.designsystem.components.brand.NexoSuccessIcon
+import dev.alejo.core.designsystem.components.buttons.NexoButton
+import dev.alejo.core.designsystem.components.buttons.NexoButtonStyle
+import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NexoSimpleSuccessLayout(
+    title: String,
+    description: String,
+    icon: @Composable () -> Unit,
+    primaryButton: @Composable () -> Unit,
+    secondaryButton: @Composable (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        icon()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = -(25).dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.extended.textPrimary,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.extended.textSecondary,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            primaryButton()
+
+            if (secondaryButton != null) {
+                secondaryButton()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NexoSimpleSuccessLayoutPreview() {
+    NexoTheme {
+        NexoSimpleSuccessLayout(
+            title = "Title",
+            description = "Description",
+            icon = {
+                NexoSuccessIcon()
+            },
+            primaryButton = {
+                NexoButton(
+                    text = "Primary Button",
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+            },
+            secondaryButton = {
+                NexoButton(
+                    text = "Secondary Button",
+                    onClick = {},
+                    style = NexoButtonStyle.SECONDARY,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        )
+    }
+}
