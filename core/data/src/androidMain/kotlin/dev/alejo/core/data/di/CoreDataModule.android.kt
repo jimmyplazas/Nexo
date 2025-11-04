550a44f3c0fefbea94ad6dd314104fc57b607c59
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-04 17:59:45 -0500

    feat: Add OkHttp engine for Android

---
+++ core/data/src/androidMain/kotlin/dev/alejo/core/data/di/CoreDataModule.android.kt
@@ -0,0 +1,9 @@
+package dev.alejo.core.data.di
+
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+import org.koin.dsl.module
+
+actual val platformCoreDataModule = module {
+    single<HttpClientEngine> { OkHttp.create() }
+}
