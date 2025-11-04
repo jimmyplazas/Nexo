9004185d8ad8701be38a18c9ad83f8ba1d9d14a0
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-04 18:01:15 -0500

    feat: Add `RegisterAction` sealed interface

--- feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/register/RegisterAction.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/register/RegisterAction.kt
@@ -1,5 +1,8 @@
 package dev.alejo.auth.presentation.register
 
 sealed interface RegisterAction {
-
+    data object OnLoginClick : RegisterAction
+    data object OnInputTextFocusGain : RegisterAction
+    data object OnRegisterClick : RegisterAction
+    data object OnTogglePasswordVisibilityClick : RegisterAction
 }
