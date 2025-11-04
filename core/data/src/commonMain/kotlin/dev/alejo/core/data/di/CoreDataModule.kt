ca7ac3662f34f942e5095498e979427435e85c09
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-04 18:02:23 -0500

    feat: Add `core-data` dependency injection module

---
+++ core/data/src/commonMain/kotlin/dev/alejo/core/data/di/CoreDataModule.kt
@@ -0,0 +1,23 @@
+package dev.alejo.core.data.di
+
+import dev.alejo.core.data.auth.KtorAuthService
+import dev.alejo.core.data.logging.KermitLogger
+import dev.alejo.core.data.networking.HttpClientFactory
+import dev.alejo.core.domain.auth.AuthService
+import dev.alejo.core.domain.logging.NexoLogger
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformCoreDataModule: Module
+
+val coreDataModule = module {
+    includes(platformCoreDataModule)
+    single<NexoLogger> { KermitLogger }
+    single {
+        HttpClientFactory(get())
+            .create(get())
+    }
+    singleOf(::KtorAuthService) bind AuthService::class
+}
