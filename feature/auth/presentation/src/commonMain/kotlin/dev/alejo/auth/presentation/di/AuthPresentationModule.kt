0bbca1af970293961cba323239af38f1d1ff7e46
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-04 17:59:32 -0500

    feat: Add auth presentation module and RegisterViewModel

---
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/di/AuthPresentationModule.kt
@@ -0,0 +1,9 @@
+package dev.alejo.auth.presentation.di
+
+import dev.alejo.auth.presentation.register.RegisterViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val authPresentationModule = module {
+    viewModelOf(::RegisterViewModel)
+}
