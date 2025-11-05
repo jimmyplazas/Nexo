49f7978f09e3452d6795fc02373a4935ca42af46
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 18:47:03 -0500

    feat: Add email verification service method

--- core/data/src/commonMain/kotlin/dev/alejo/core/data/auth/KtorAuthService.kt
+++ core/data/src/commonMain/kotlin/dev/alejo/core/data/auth/KtorAuthService.kt
@@ -2,6 +2,7 @@ package dev.alejo.core.data.auth
 
 import dev.alejo.core.data.dto.requests.EmailRequest
 import dev.alejo.core.data.dto.requests.RegisterRequest
+import dev.alejo.core.data.networking.get
 import dev.alejo.core.data.networking.post
 import dev.alejo.core.domain.EmptyResult
 import dev.alejo.core.domain.auth.AuthService
@@ -11,6 +12,7 @@ import io.ktor.client.HttpClient
 class KtorAuthService(
     private val httpClient: HttpClient
 ) : AuthService {
+
     override suspend fun register(
         email: String,
         username: String,
@@ -26,12 +28,18 @@ class KtorAuthService(
         )
     }
 
-    override suspend fun resendVerificationEmail(
-        email: String
-    ): EmptyResult<DataError.Remote> {
+    override suspend fun resendVerificationEmail(email: String): EmptyResult<DataError.Remote> {
         return httpClient.post(
             route = "/auth/resend-verification",
             body = EmailRequest(email = email)
         )
     }
+
+    override suspend fun verifyEmail(token: String): EmptyResult<DataError.Remote> {
+        return httpClient.get(
+            route = "/auth/verify",
+            queryParams = mapOf("token" to token)
+        )
+    }
+
 }
