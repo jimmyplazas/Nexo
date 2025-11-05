8702478d20aed8d4170cfed06e797724e9797d5b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 15:58:19 -0500

    feat: Integrate navigation module

--- feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/di/AuthPresentationModule.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/di/AuthPresentationModule.kt
@@ -1,9 +1,11 @@
 package dev.alejo.auth.presentation.di
 
 import dev.alejo.auth.presentation.register.RegisterViewModel
+import dev.alejo.auth.presentation.register_success.RegisterSuccessViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val authPresentationModule = module {
     viewModelOf(::RegisterViewModel)
+    viewModelOf(::RegisterSuccessViewModel)
 }
