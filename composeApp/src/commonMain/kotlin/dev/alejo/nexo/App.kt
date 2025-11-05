7ab72563e89f73d4f96ffba88d3217113b642ce8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 18:45:52 -0500

    feat: Add `NavController` and `DeepLinkListener` to `App` Composable

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
@@ -1,14 +1,18 @@
 package dev.alejo.nexo
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.nexo.navigation.DeepLinkListener
 import dev.alejo.nexo.navigation.NavigationRoot
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    val navController = rememberNavController()
+    DeepLinkListener(navController)
     NexoTheme {
-        NavigationRoot()
+        NavigationRoot(navController)
     }
 }
