9eae3b8a713830b9febacdd32f63064475e9f0ca
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 15:58:02 -0500

    feat: Integrate navigation module

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
@@ -1,14 +1,14 @@
 package dev.alejo.nexo
 
 import androidx.compose.runtime.Composable
-import dev.alejo.auth.presentation.register.RegisterRoot
 import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.nexo.navigation.NavigationRoot
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     NexoTheme {
-        RegisterRoot() {}
+        NavigationRoot()
     }
 }
