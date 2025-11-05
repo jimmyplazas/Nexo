4cf9086fbd7df7ab1291172029284f594b477759
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 18:46:00 -0500

    feat: Add email verification screen and deep links

--- feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/navigation/AuthGraph.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/navigation/AuthGraph.kt
@@ -3,7 +3,9 @@ package dev.alejo.auth.presentation.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import androidx.navigation.navigation
+import dev.alejo.auth.presentation.email_verification.EmailVerificationRoot
 import dev.alejo.auth.presentation.register.RegisterRoot
 import dev.alejo.auth.presentation.register_success.RegisterSuccessRoot
 
@@ -23,6 +25,18 @@ fun NavGraphBuilder.authGraph(
         }
         composable<AuthGraphRoutes.RegisterSuccess> {
             RegisterSuccessRoot()
+        }
+        composable<AuthGraphRoutes.EmailVerification>(
+            deepLinks = listOf(
+                navDeepLink {
+                    this.uriPattern = "https://nexo.jimmyplazas.dev/api/auth/verify?token={token}"
+                },
+                navDeepLink {
+                    this.uriPattern = "nexo://nexo.jimmyplazas.dev/api/auth/verify?token={token}"
+                },
+            )
+        ) {
+            EmailVerificationRoot()
         }
     }
 }
