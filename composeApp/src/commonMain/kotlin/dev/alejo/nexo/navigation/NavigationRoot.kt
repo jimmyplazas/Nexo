dfb68f9d31d60fe279115f44a74de8908a4de048
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 18:47:10 -0500

    refactor: Hoist NavController in NavigationRoot

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/navigation/NavigationRoot.kt
@@ -1,17 +1,15 @@
 package dev.alejo.nexo.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import dev.alejo.auth.presentation.navigation.AuthGraphRoutes
 import dev.alejo.auth.presentation.navigation.authGraph
 
 @Composable
 fun NavigationRoot(
-    modifier: Modifier = Modifier
+    navController: NavHostController
 ) {
-    val navController = rememberNavController()
     NavHost(
         navController = navController,
         startDestination = AuthGraphRoutes.Graph
