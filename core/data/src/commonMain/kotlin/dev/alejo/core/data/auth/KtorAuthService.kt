36dda7006b51c8295e0e57d1dcad61afdd6304dc
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 14:20:01 -0500

    feat: Update login to return `AuthInfo`

--- core/data/src/commonMain/kotlin/dev/alejo/core/data/auth/KtorAuthService.kt
+++ core/data/src/commonMain/kotlin/dev/alejo/core/data/auth/KtorAuthService.kt
@@ -1,18 +1,38 @@
 package dev.alejo.core.data.auth
 
+import dev.alejo.core.data.dto.AuthInfoSerializable
 import dev.alejo.core.data.dto.requests.EmailRequest
 import dev.alejo.core.data.dto.requests.LoginRequest
 import dev.alejo.core.data.dto.requests.RegisterRequest
+import dev.alejo.core.data.mappers.toDomain
 import dev.alejo.core.data.networking.get
 import dev.alejo.core.data.networking.post
 import dev.alejo.core.domain.EmptyResult
+import dev.alejo.core.domain.Result
+import dev.alejo.core.domain.auth.AuthInfo
 import dev.alejo.core.domain.auth.AuthService
+import dev.alejo.core.domain.map
 import dev.alejo.core.domain.util.DataError
 import io.ktor.client.HttpClient
 
 class KtorAuthService(
     private val httpClient: HttpClient
 ) : AuthService {
+
+    override suspend fun login(
+        email: String,
+        password: String
+    ): Result<AuthInfo, DataError.Remote> {
+        return httpClient.post< LoginRequest, AuthInfoSerializable>(
+            route = "/auth/login",
+            body = LoginRequest(
+                email = email,
+                password = password
+            )
+        ).map { authInfoSerializable ->
+            authInfoSerializable.toDomain()
+        }
+    }
 
     override suspend fun register(
         email: String,
@@ -40,19 +60,6 @@ class KtorAuthService(
         return httpClient.get(
             route = "/auth/verify",
             queryParams = mapOf("token" to token)
-        )
-    }
-
-    override suspend fun login(
-        email: String,
-        password: String
-    ): EmptyResult<DataError.Remote> {
-        return httpClient.post(
-            route = "/auth/login",
-            body = LoginRequest(
-                email = email,
-                password = password
-            )
         )
     }
 
