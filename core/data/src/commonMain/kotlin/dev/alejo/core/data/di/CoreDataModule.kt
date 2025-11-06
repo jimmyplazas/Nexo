ac5cee3e497d9070ca747a4734b062fbff81f875
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 15:22:49 -0500

    feat: Add `DataStoreSessionStorage` to DI graph

--- core/data/src/commonMain/kotlin/dev/alejo/core/data/di/CoreDataModule.kt
+++ core/data/src/commonMain/kotlin/dev/alejo/core/data/di/CoreDataModule.kt
@@ -1,9 +1,11 @@
 package dev.alejo.core.data.di
 
+import dev.alejo.core.data.auth.DataStoreSessionStorage
 import dev.alejo.core.data.auth.KtorAuthService
 import dev.alejo.core.data.logging.KermitLogger
 import dev.alejo.core.data.networking.HttpClientFactory
 import dev.alejo.core.domain.auth.AuthService
+import dev.alejo.core.domain.auth.SessionStorage
 import dev.alejo.core.domain.logging.NexoLogger
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -16,8 +18,9 @@ val coreDataModule = module {
     includes(platformCoreDataModule)
     single<NexoLogger> { KermitLogger }
     single {
-        HttpClientFactory(get())
+        HttpClientFactory(get(), get())
             .create(get())
     }
     singleOf(::KtorAuthService) bind AuthService::class
+    singleOf(::DataStoreSessionStorage) bind SessionStorage::class
 }
