838b29d0f72a26b3631120ae0edf993cd36307c8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 15:22:56 -0500

    feat(auth): Add automatic token refresh</h6>

--- core/data/src/commonMain/kotlin/dev/alejo/core/data/networking/HttpClientFactory.kt
+++ core/data/src/commonMain/kotlin/dev/alejo/core/data/networking/HttpClientFactory.kt
@@ -1,10 +1,19 @@
 package dev.alejo.core.data.networking
 
 import dev.alejo.core.data.BuildKonfig
+import dev.alejo.core.data.dto.AuthInfoSerializable
+import dev.alejo.core.data.dto.requests.RefreshRequest
+import dev.alejo.core.data.mappers.toDomain
+import dev.alejo.core.domain.auth.SessionStorage
 import dev.alejo.core.domain.logging.NexoLogger
+import dev.alejo.core.domain.onFailure
+import dev.alejo.core.domain.onSuccess
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
@@ -12,9 +21,11 @@ import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.request.header
+import io.ktor.client.statement.request
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.serialization.json.Json
 
 /**
@@ -28,7 +39,8 @@ import kotlinx.serialization.json.Json
  * @property nexoLogger A custom logger interface for logging HTTP requests and responses.
  */
 class HttpClientFactory(
-    private val nexoLogger: NexoLogger
+    private val nexoLogger: NexoLogger,
+    private val sessionStorage: SessionStorage
 ) {
 
     fun create(engine: HttpClientEngine): HttpClient {
@@ -62,6 +74,55 @@ class HttpClientFactory(
             defaultRequest {
                 header("x-api-key", BuildKonfig.API_KEY)
                 contentType(ContentType.Application.Json)
+            }
+
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        sessionStorage
+                            .observeAuthInf()
+                            .firstOrNull()
+                            ?.let {
+                                BearerTokens(
+                                    accessToken = it.accessToken,
+                                    refreshToken = it.refreshToken
+                                )
+                            }
+                    }
+                    refreshTokens {
+                        if (response.request.url.encodedPath.contains("auth/")) {
+                            return@refreshTokens null
+                        }
+
+                        val authInfo = sessionStorage.observeAuthInf().firstOrNull()
+                        if (authInfo?.refreshToken.isNullOrBlank()) {
+                            sessionStorage.set(null)
+                            return@refreshTokens null
+                        }
+
+                        var bearerTokens: BearerTokens? = null
+
+                        client.post<RefreshRequest, AuthInfoSerializable>(
+                            route = "/auth/refresh",
+                            body = RefreshRequest(
+                                refreshToken = authInfo.refreshToken
+                            ),
+                            builder = {
+                                markAsRefreshTokenRequest()
+                            }
+                        ).onSuccess { newAuthInfo ->
+                            sessionStorage.set(newAuthInfo.toDomain())
+                            bearerTokens = BearerTokens(
+                                accessToken = newAuthInfo.accessToken,
+                                refreshToken = newAuthInfo.refreshToken
+                            )
+                        }.onFailure { error ->
+                            sessionStorage.set(null)
+                        }
+
+                        bearerTokens
+                    }
+                }
             }
         }
     }
