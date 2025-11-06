922a609ae72d0ddfdf8a0385aa592e52743f66c4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 14:20:47 -0500

    feat: Navigate to chat list on login success

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/navigation/NavigationRoot.kt
@@ -3,8 +3,11 @@ package dev.alejo.nexo.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import dev.alejo.auth.presentation.navigation.AuthGraphRoutes
 import dev.alejo.auth.presentation.navigation.authGraph
+import dev.alejo.chat.presentation.chat_list.ChatListRoute
+import dev.alejo.chat.presentation.chat_list.ChatListScreenRoot
 
 @Composable
 fun NavigationRoot(
@@ -17,8 +20,15 @@ fun NavigationRoot(
         authGraph(
             navController = navController,
             onLoginSuccess = {
-
+                navController.navigate(ChatListRoute) {
+                    popUpTo(AuthGraphRoutes.Graph) {
+                        inclusive = true
+                    }
+                }
             }
         )
+        composable<ChatListRoute> {
+            ChatListScreenRoot()
+        }
     }
 }
