9caf95f2fe4ca21bbc93a3b9ecaebea76216f945
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 14:19:45 -0500

    feat: Update `login` function in `AuthService` to return `AuthInfo`

--- core/domain/src/commonMain/kotlin/dev/alejo/core/domain/auth/AuthService.kt
+++ core/domain/src/commonMain/kotlin/dev/alejo/core/domain/auth/AuthService.kt
@@ -1,9 +1,14 @@
 package dev.alejo.core.domain.auth
 
 import dev.alejo.core.domain.EmptyResult
+import dev.alejo.core.domain.Result
 import dev.alejo.core.domain.util.DataError
 
 interface AuthService {
+    suspend fun login(
+        email: String,
+        password: String
+    ): Result<AuthInfo, DataError.Remote>
     suspend fun register(
         email: String,
         username: String,
@@ -11,5 +16,4 @@ interface AuthService {
     ): EmptyResult<DataError.Remote>
     suspend fun resendVerificationEmail(email: String): EmptyResult<DataError.Remote>
     suspend fun verifyEmail(token: String): EmptyResult<DataError.Remote>
-    suspend fun login(email: String, password: String): EmptyResult<DataError.Remote>
 }
