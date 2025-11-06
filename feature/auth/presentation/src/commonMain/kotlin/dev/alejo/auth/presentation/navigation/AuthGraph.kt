647fcb609c5eca5842eebc4f6df93cc62b6badba
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-05 21:46:55 -0500

    feat: Add Login screen to auth graph and set as start destination

--- feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/navigation/AuthGraph.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/navigation/AuthGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navDeepLink
 import androidx.navigation.navigation
 import dev.alejo.auth.presentation.email_verification.EmailVerificationRoot
+import dev.alejo.auth.presentation.login.LoginRoot
 import dev.alejo.auth.presentation.register.RegisterRoot
 import dev.alejo.auth.presentation.register_success.RegisterSuccessRoot
 
@@ -14,12 +15,37 @@ fun NavGraphBuilder.authGraph(
     onLoginSuccess: () -> Unit
 ) {
     navigation<AuthGraphRoutes.Graph>(
-        startDestination = AuthGraphRoutes.Register
+        startDestination = AuthGraphRoutes.Login
     ) {
+        composable<AuthGraphRoutes.Login> {
+            LoginRoot(
+                onLoginSuccess = onLoginSuccess,
+                onForgotPassword = {
+                    navController.navigate(AuthGraphRoutes.ForgotPassword)
+                },
+                onCreateAccount = {
+                    navController.navigate(AuthGraphRoutes.Register) {
+                        popUpTo(AuthGraphRoutes.Login)
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
         composable<AuthGraphRoutes.Register> {
             RegisterRoot(
                 onRegisterSuccess = { email ->
                     navController.navigate(AuthGraphRoutes.RegisterSuccess(email))
+                },
+                onLoginClick = {
+                    navController.navigate(AuthGraphRoutes.Login) {
+                        popUpTo(AuthGraphRoutes.Register) {
+                            inclusive = true
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 }
             )
         }
