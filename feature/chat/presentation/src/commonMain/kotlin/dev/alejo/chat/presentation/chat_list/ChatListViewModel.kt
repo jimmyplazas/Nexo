95746741cb0143f2f84354222b14c08a62b0568d
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 19:16:32 -0500

    feat: Add `ChatListViewModel`

---
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_list/ChatListViewModel.kt
@@ -0,0 +1,10 @@
+package dev.alejo.chat.presentation.chat_list
+
+import androidx.lifecycle.ViewModel
+import dev.alejo.core.domain.auth.SessionStorage
+
+class ChatListViewModel(
+    private val sessionStorage: SessionStorage,
+) : ViewModel() {
+
+}
