8a76d52b01566abd2c10d4a50e867427c74326bd
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-07 13:46:36 -0500

    feat: Add `NexoMultiLineTextField` component

---
+++ core/designsystem/src/commonMain/kotlin/dev/alejo/core/designsystem/components/textfields/NexoMultiLineTextField.kt
@@ -0,0 +1,108 @@
+package dev.alejo.core.designsystem.components.textfields
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.unit.dp
+import dev.alejo.core.designsystem.components.buttons.NexoButton
+import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NexoMultiLineTextField(
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    enabled: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    onKeyboardActions: () -> Unit = {},
+    bottomContent: @Composable (RowScope.() -> Unit)? = null
+) {
+    Column(
+        modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.extended.surfaceLower,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.extended.surfaceOutline,
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(vertical = 12.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        BasicTextField(
+            state = state,
+            modifier = Modifier
+                .weight(1f),
+            enabled = enabled,
+            textStyle = MaterialTheme.typography.bodyLarge.copy(
+                color = MaterialTheme.colorScheme.extended.textPrimary
+            ),
+            keyboardOptions = keyboardOptions,
+            onKeyboardAction = { onKeyboardActions() },
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.extended.textPrimary),
+            decorator = { innerBox ->
+                if (placeholder != null && state.text.isNotEmpty()) {
+                    Text(
+                        text = placeholder,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                    )
+                }
+                innerBox
+            }
+        )
+
+        if (bottomContent != null) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                bottomContent(this)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NexoMultiLineTextFieldPreview() {
+    NexoTheme {
+        NexoMultiLineTextField(
+            state = rememberTextFieldState(
+                initialText = "This is some textfield content that maybe spans multiple lines"
+            ),
+            modifier = Modifier.widthIn(300.dp),
+            bottomContent = {
+                Spacer(Modifier.weight(1f))
+                NexoButton(
+                    text = "Send",
+                    onClick = { }
+                )
+            }
+        )
+    }
+}
