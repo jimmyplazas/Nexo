16909f57b1614c1f9dba3c39d11c592887822ca2
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 19:16:43 -0500

    feat: Add ChatPresentationModule for DI

---
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/di/ChatPresentationModule.kt
@@ -0,0 +1,9 @@
+package dev.alejo.chat.presentation.di
+
+import dev.alejo.chat.presentation.chat_list.ChatListViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val chatPresentationModule = module {
+    viewModelOf(::ChatListViewModel)
+}
