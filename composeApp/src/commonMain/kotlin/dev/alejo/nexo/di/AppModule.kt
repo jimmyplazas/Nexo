2f395e6859f247c9e7d03204320e43ab536b09d9
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 19:16:26 -0500

    feat: Add `appModule` with Koin for DI

---
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/di/AppModule.kt
@@ -0,0 +1,9 @@
+package dev.alejo.nexo.di
+
+import dev.alejo.nexo.MainViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val appModule = module {
+    viewModelOf(::MainViewModel)
+}
