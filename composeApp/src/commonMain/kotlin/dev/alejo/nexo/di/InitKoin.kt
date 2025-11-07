147cc1d8725a506e5ce458288b15f0bae4928fbb
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-07 15:38:14 -0500

    feat: Introduce core presentation module

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/di/InitKoin.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/di/InitKoin.kt
@@ -3,6 +3,7 @@ package dev.alejo.nexo.di
 import dev.alejo.auth.presentation.di.authPresentationModule
 import dev.alejo.chat.presentation.di.chatPresentationModule
 import dev.alejo.core.data.di.coreDataModule
+import dev.alejo.core.presentation.di.corePresentationModule
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
@@ -13,7 +14,8 @@ fun initKoin(config: KoinAppDeclaration? = null) {
             coreDataModule,
             authPresentationModule,
             appModule,
-            chatPresentationModule
+            chatPresentationModule,
+            corePresentationModule
         )
     }
 }
