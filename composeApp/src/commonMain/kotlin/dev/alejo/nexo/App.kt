66a899a9605eabdd30af49a53b674d794080d693
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 19:14:51 -0500

    feat: Check authentication status and handle session expiry

--- composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
+++ composeApp/src/commonMain/kotlin/dev/alejo/nexo/App.kt
@@ -1,18 +1,58 @@
 package dev.alejo.nexo
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
+import dev.alejo.auth.presentation.navigation.AuthGraphRoutes
+import dev.alejo.chat.presentation.chat_list.ChatListRoute
 import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.presentation.util.ObserveAsEvents
 import dev.alejo.nexo.navigation.DeepLinkListener
 import dev.alejo.nexo.navigation.NavigationRoot
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    onAuthenticationChecked: () -> Unit = { },
+    viewModel: MainViewModel = koinViewModel()
+) {
     val navController = rememberNavController()
     DeepLinkListener(navController)
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(state.isCheckingAuth) {
+        if (!state.isCheckingAuth) {
+            onAuthenticationChecked()
+        }
+    }
+
+    ObserveAsEvents(viewModel.events) { event ->
+        when (event) {
+            MainEvent.OnSessionExpired -> {
+                navController.navigate(AuthGraphRoutes.Graph) {
+                    popUpTo(AuthGraphRoutes.Graph) {
+                        inclusive = false
+                    }
+                }
+            }
+        }
+    }
+
     NexoTheme {
-        NavigationRoot(navController)
+        if (!state.isCheckingAuth) {
+            NavigationRoot(
+                navController = navController,
+                startDestination = if (state.isLoggedIn) {
+                    ChatListRoute
+                } else {
+                    AuthGraphRoutes.Graph
+                }
+            )
+        }
     }
 }
