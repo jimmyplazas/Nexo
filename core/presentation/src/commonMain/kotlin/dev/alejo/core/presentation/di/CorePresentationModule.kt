6f91a6cd2a3c2453330962f1b3a4d81241514d2d
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-07 15:38:22 -0500

    feat: Add `core-presentation` module and `ScopedStoreRegistryViewModel`

---
+++ core/presentation/src/commonMain/kotlin/dev/alejo/core/presentation/di/CorePresentationModule.kt
@@ -0,0 +1,9 @@
+package dev.alejo.core.presentation.di
+
+import dev.alejo.core.presentation.util.ScopedStoreRegistryViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val corePresentationModule = module {
+    viewModelOf(::ScopedStoreRegistryViewModel)
+}
