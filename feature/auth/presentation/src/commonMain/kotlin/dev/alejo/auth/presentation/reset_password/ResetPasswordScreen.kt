735c37be53b4d43253e4b25f171a5e7029738e90
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-07 09:44:06 -0500

    refactor: Wrap `ResetPasswordScreen` content in `NexoScaffold`

--- feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/reset_password/ResetPasswordScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/reset_password/ResetPasswordScreen.kt
@@ -14,6 +14,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.alejo.core.designsystem.components.brand.NexoBrandLogo
 import dev.alejo.core.designsystem.components.buttons.NexoButton
 import dev.alejo.core.designsystem.components.layout.NexoAdaptiveFormLayout
+import dev.alejo.core.designsystem.components.layout.NexoScaffold
 import dev.alejo.core.designsystem.components.textfields.NexoPasswordTextField
 import dev.alejo.core.designsystem.theme.NexoTheme
 import dev.alejo.core.designsystem.theme.extended
@@ -45,43 +46,45 @@ fun ResetPasswordScreen(
     state: ResetPasswordState,
     onAction: (ResetPasswordAction) -> Unit,
 ) {
-    NexoAdaptiveFormLayout(
-        headerText = stringResource(Res.string.set_new_password),
-        errorText = state.errorText?.asString(),
-        logo = { NexoBrandLogo() }
-    ) {
-        NexoPasswordTextField(
-            state = state.passwordTextState,
-            title = stringResource(Res.string.new_password),
-            placeholder = stringResource(Res.string.password),
-            supportingText = stringResource(Res.string.password_hint),
-            isPasswordVisible = state.isPasswordVisible,
-            onToggleVisibilityClick = {
-                onAction(ResetPasswordAction.OnTogglePasswordVisibilityClick)
-            }
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        NexoButton(
-            text = stringResource(Res.string.submit),
-            onClick = {
-                onAction(ResetPasswordAction.OnSubmitClick)
-            },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = state.canSubmit && !state.isLoading,
-            isLoading = state.isLoading
-        )
-
-        if (state.isResetSuccessful) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = stringResource(Res.string.forgot_password_email_sent_successfully),
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.extended.success,
-                textAlign = TextAlign.Center
+    NexoScaffold {
+        NexoAdaptiveFormLayout(
+            headerText = stringResource(Res.string.set_new_password),
+            errorText = state.errorText?.asString(),
+            logo = { NexoBrandLogo() }
+        ) {
+            NexoPasswordTextField(
+                state = state.passwordTextState,
+                title = stringResource(Res.string.new_password),
+                placeholder = stringResource(Res.string.password),
+                supportingText = stringResource(Res.string.password_hint),
+                isPasswordVisible = state.isPasswordVisible,
+                onToggleVisibilityClick = {
+                    onAction(ResetPasswordAction.OnTogglePasswordVisibilityClick)
+                }
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            NexoButton(
+                text = stringResource(Res.string.submit),
+                onClick = {
+                    onAction(ResetPasswordAction.OnSubmitClick)
+                },
+                modifier = Modifier.fillMaxWidth(),
+                enabled = state.canSubmit && !state.isLoading,
+                isLoading = state.isLoading
+            )
+
+            if (state.isResetSuccessful) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(Res.string.forgot_password_email_sent_successfully),
+                    modifier = Modifier.fillMaxWidth(),
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.extended.success,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
