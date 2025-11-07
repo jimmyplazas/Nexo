c22812470bc014fa2470d4dafd73a1f347d23ad9
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-06 19:54:06 -0500

    feat: Add `ResetPasswordViewModel` to presentation module

--- feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/di/AuthPresentationModule.kt
+++ feature/auth/presentation/src/commonMain/kotlin/dev/alejo/auth/presentation/di/AuthPresentationModule.kt
@@ -5,6 +5,7 @@ import dev.alejo.auth.presentation.forgot_password.ForgotPasswordViewModel
 import dev.alejo.auth.presentation.login.LoginViewModel
 import dev.alejo.auth.presentation.register.RegisterViewModel
 import dev.alejo.auth.presentation.register_success.RegisterSuccessViewModel
+import dev.alejo.auth.presentation.reset_password.ResetPasswordViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
@@ -14,4 +15,5 @@ val authPresentationModule = module {
     viewModelOf(::EmailVerificationViewModel)
     viewModelOf(::LoginViewModel)
     viewModelOf(::ForgotPasswordViewModel)
+    viewModelOf(::ResetPasswordViewModel)
 }
