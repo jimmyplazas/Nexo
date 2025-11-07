a379cb9188b805d3f47a484884fe6fa113a084b0
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-07 17:57:18 -0500

    feat: Add `ChatListDetailViewModel` to Koin module

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/di/ChatPresentationModule.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/di/ChatPresentationModule.kt
@@ -1,9 +1,11 @@
 package dev.alejo.chat.presentation.di
 
 import dev.alejo.chat.presentation.chat_list.ChatListViewModel
+import dev.alejo.chat.presentation.chat_list_detail.ChatListDetailViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val chatPresentationModule = module {
     viewModelOf(::ChatListViewModel)
+    viewModelOf(::ChatListDetailViewModel)
 }
