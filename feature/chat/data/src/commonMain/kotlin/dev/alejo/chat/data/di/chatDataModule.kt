1bb489f038c3c87429a015d5c7ea0bee9a932664
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 16:13:12 -0500

    feat: Add ChatService to chat data module

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
@@ -1,11 +1,14 @@
 package dev.alejo.chat.data.di
 
 import dev.alejo.chat.data.chat.KtorChatParticipantService
+import dev.alejo.chat.data.chat.KtorChatService
 import dev.alejo.chat.domain.chat.ChatParticipantService
+import dev.alejo.chat.domain.chat.ChatService
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val chatDataModule = module {
     singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
+    singleOf(::KtorChatService) bind ChatService::class
 }
