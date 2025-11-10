4fc2cd0e79551507e21551c97c74f8537fce61a8
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 14:38:46 -0500

    refactor(chat): Rename state fields in CreateChatState

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/create_chat/CreateChatState.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/create_chat/CreateChatState.kt
@@ -7,9 +7,8 @@ import dev.alejo.core.presentation.util.UiText
 data class CreateChatState(
     val queryTextState: TextFieldState = TextFieldState(),
     val selectedChatParticipants: List<ChatParticipantUi> = emptyList(),
-    val isAddingParticipant: Boolean = false,
-    val isLoadingParticipants: Boolean = false,
-    val canAddParticipants: Boolean = false,
+    val isSearching: Boolean = false,
+    val canAddParticipant: Boolean = false,
     val currentSearchResult: ChatParticipantUi? = null,
     val searchError: UiText? = null,
     val isCreatingChat: Boolean = false,
