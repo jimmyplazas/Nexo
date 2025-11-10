3327e2d6e0b99c211f7901e1cc9d80555e1520d4
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 16:14:44 -0500

    feat: Add chat message DTO to domain mapper

---
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/mappers/ChatMessageMappers.kt
@@ -0,0 +1,15 @@
+package dev.alejo.chat.data.mappers
+
+import dev.alejo.chat.data.dto.ChatMessageDto
+import dev.alejo.chat.domain.models.ChatMessage
+import kotlin.time.Instant
+
+fun ChatMessageDto.toDomain(): ChatMessage {
+    return ChatMessage(
+        id = id,
+        chatId = chatId,
+        content = content,
+        createdAt = Instant.parse(createdAt),
+        senderId = senderId
+    )
+}
