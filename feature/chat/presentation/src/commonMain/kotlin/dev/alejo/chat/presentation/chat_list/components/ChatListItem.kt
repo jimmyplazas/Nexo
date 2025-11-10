2e535ede630382bd147bd97a562d8ceb3cbd3889
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 18:38:45 -0500

    feat: Add `ChatListItem` composable

---
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_list/components/ChatListItem.kt
@@ -0,0 +1,291 @@
+package dev.alejo.chat.presentation.chat_list.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import dev.alejo.chat.domain.models.ChatMessage
+import dev.alejo.chat.presentation.model.ChatUi
+import dev.alejo.core.designsystem.components.avatar.ChatParticipantUi
+import dev.alejo.core.designsystem.components.avatar.NexoStackedAvatars
+import dev.alejo.core.designsystem.theme.NexoTheme
+import dev.alejo.core.designsystem.theme.extended
+import dev.alejo.core.designsystem.theme.titleXSmall
+import nexo.feature.chat.presentation.generated.resources.Res
+import nexo.feature.chat.presentation.generated.resources.group_chat
+import nexo.feature.chat.presentation.generated.resources.you
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.time.Clock
+
+@Composable
+fun ChatListItem(
+    chat: ChatUi,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+
+    val isGroupChat = chat.otherParticipants.size > 1
+    Row(
+        modifier = modifier
+            .height(IntrinsicSize.Min)
+            .background(
+                color = if (isSelected) {
+                    MaterialTheme.colorScheme.surface
+                } else {
+                    MaterialTheme.colorScheme.extended.surfaceLower
+                }
+            )
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                NexoStackedAvatars(
+                    avatars = chat.otherParticipants
+                )
+                Column(
+                    modifier = Modifier
+                        .weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    Text(
+                        text = if (!isGroupChat) {
+                            chat.otherParticipants.first().username
+                        } else {
+                            stringResource(Res.string.group_chat)
+                        },
+                        style = MaterialTheme.typography.titleXSmall,
+                        color = MaterialTheme.colorScheme.extended.textPrimary,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    if (isGroupChat) {
+                        val you = stringResource(Res.string.you)
+                        val formattedUsernames = remember(chat.otherParticipants) {
+                            you + chat.otherParticipants.joinToString { it.username }
+                        }
+                        Text(
+                            text = formattedUsernames,
+                            color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.fillMaxWidth(),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+            }
+            chat.lastMessage?.let { lastMessage ->
+                val lastMessagePreview = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
+                        append(chat.lastMessageSenderUsername + ": ")
+                    }
+                    append(lastMessage.content)
+                }
+
+                Text(
+                    text = lastMessagePreview,
+                    color = MaterialTheme.colorScheme.extended.textSecondary,
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.fillMaxWidth(),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .alpha(if (isSelected) 1f else 0f)
+                .background(MaterialTheme.colorScheme.primary)
+                .width(4.dp)
+                .fillMaxHeight()
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ChatListItemUiPreview() {
+    NexoTheme {
+        ChatListItem(
+            chat = ChatUi(
+                id = "1",
+                localParticipant = ChatParticipantUi(
+                    id = "1",
+                    username = "alejo",
+                    initials = "AL"
+                ),
+                otherParticipants = listOf(
+                    ChatParticipantUi(
+                        id = "2",
+                        username = "Luz",
+                        initials = "LU"
+                    ),
+                    ChatParticipantUi(
+                        id = "3",
+                        username = "Pin",
+                        initials = "PI"
+                    )
+                )
+            ),
+            isSelected = true
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ChatListItemUiDarkPreview() {
+    NexoTheme(darkTheme = true) {
+        ChatListItem(
+            chat = ChatUi(
+                id = "1",
+                localParticipant = ChatParticipantUi(
+                    id = "1",
+                    username = "alejo",
+                    initials = "AL"
+                ),
+                otherParticipants = listOf(
+                    ChatParticipantUi(
+                        id = "2",
+                        username = "Luz",
+                        initials = "LU"
+                    ),
+                    ChatParticipantUi(
+                        id = "3",
+                        username = "Pin",
+                        initials = "PI"
+                    )
+                ),
+                lastMessage = ChatMessage(
+                    id = "1",
+                    chatId = "2",
+                    senderId = "2",
+                    content = "Hey mate",
+                    createdAt = Clock.System.now()
+                ),
+                lastMessageSenderUsername = "Pin"
+            ),
+            isSelected = true
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ChatListItemUiDarkManyPreview() {
+    NexoTheme(darkTheme = true) {
+        ChatListItem(
+            chat = ChatUi(
+                id = "1",
+                localParticipant = ChatParticipantUi(
+                    id = "1",
+                    username = "alejo",
+                    initials = "AL"
+                ),
+                otherParticipants = listOf(
+                    ChatParticipantUi(
+                        id = "2",
+                        username = "Luz",
+                        initials = "LU"
+                    ),
+                    ChatParticipantUi(
+                        id = "3",
+                        username = "Pin",
+                        initials = "PI"
+                    ),
+                    ChatParticipantUi(
+                        id = "3",
+                        username = "Josh",
+                        initials = "Jo"
+                    ),
+                    ChatParticipantUi(
+                        id = "4",
+                        username = "Philipp",
+                        initials = "PH"
+                    )
+                ),
+                lastMessage = ChatMessage(
+                    id = "1",
+                    chatId = "2",
+                    senderId = "2",
+                    content = "Hey mate",
+                    createdAt = Clock.System.now()
+                ),
+                lastMessageSenderUsername = "Pin"
+            ),
+            isSelected = true
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ChatListItemUiDarkOneToOnePreview() {
+    NexoTheme(darkTheme = true) {
+        ChatListItem(
+            chat = ChatUi(
+                id = "1",
+                localParticipant = ChatParticipantUi(
+                    id = "1",
+                    username = "alejo",
+                    initials = "AL"
+                ),
+                otherParticipants = listOf(
+                    ChatParticipantUi(
+                        id = "2",
+                        username = "Luz",
+                        initials = "LU"
+                    )
+                ),
+                lastMessage = ChatMessage(
+                    id = "1",
+                    chatId = "2",
+                    senderId = "2",
+                    content = "All good",
+                    createdAt = Clock.System.now()
+                ),
+                lastMessageSenderUsername = "Luz"
+            ),
+            isSelected = true
+        )
+    }
+}
