76e23771d8154e2666591cbea9d99d0100d4ba42
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 14:39:10 -0500

    feat: Add chat data dependency injection module

---
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
@@ -0,0 +1,11 @@
+package dev.alejo.chat.data.di
+
+import dev.alejo.chat.data.chat.KtorChatParticipantService
+import dev.alejo.chat.domain.chat.ChatParticipantService
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val chatDataModule = module {
+    singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
+}
