783f84357afca3f92c110a4cba4bc23d10a74a7b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 16:14:35 -0500

    feat: Add `Chat` data-to-domain mapper

---
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/mappers/ChatMappers.kt
@@ -0,0 +1,14 @@
+package dev.alejo.chat.data.mappers
+
+import dev.alejo.chat.data.dto.ChatDto
+import dev.alejo.chat.domain.models.Chat
+import kotlin.time.Instant
+
+fun ChatDto.toDomain(): Chat {
+    return Chat(
+        id = id,
+        participants = participants.map { it.toDomain() },
+        lastActivityAt = Instant.parse(lastActivityAt),
+        lastMessage = lastMessage?.toDomain()
+    )
+}
