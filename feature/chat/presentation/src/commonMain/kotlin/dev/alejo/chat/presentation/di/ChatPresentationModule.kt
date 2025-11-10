48c4dd66af52af7d55880dd670cf7c5c921effd3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-10 13:04:56 -0500

    feat: Add `CreateChatViewModel` to presentation module

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/di/ChatPresentationModule.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/di/ChatPresentationModule.kt
@@ -2,10 +2,12 @@ package dev.alejo.chat.presentation.di
 
 import dev.alejo.chat.presentation.chat_list.ChatListViewModel
 import dev.alejo.chat.presentation.chat_list_detail.ChatListDetailViewModel
+import dev.alejo.chat.presentation.create_chat.CreateChatViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val chatPresentationModule = module {
     viewModelOf(::ChatListViewModel)
     viewModelOf(::ChatListDetailViewModel)
+    viewModelOf(::CreateChatViewModel)
 }
