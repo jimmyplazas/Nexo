6e334b25103376c8b68e63b8d7ecefcbce5b7421
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-12 13:41:35 -0500

    feat(designsystem): Set max lines for `NexoMultiLineTextField`

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/components/OtherUserMessageUi.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/components/OtherUserMessageUi.kt
@@ -1,13 +1,15 @@
 package dev.alejo.chat.presentation.chat_detail.components
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.alejo.chat.presentation.model.MessageUi
+import dev.alejo.chat.presentation.util.getChatBubbleColorForUser
 import dev.alejo.core.designsystem.components.avatar.ChatParticipantUi
 import dev.alejo.core.designsystem.components.avatar.NexoAvatarPhoto
 import dev.alejo.core.designsystem.components.chat.CornerCurvePosition
@@ -20,22 +22,24 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun OtherUserMessageUi(
     message: MessageUi.OtherUserMessage,
+    color: Color,
     modifier: Modifier = Modifier
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth(),
         verticalAlignment = Alignment.Bottom,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         NexoAvatarPhoto(
             displayText = message.sender.initials,
-            imageUrl = message.sender.imageUrl
+            imageUrl = message.sender.imageUrl,
+            modifier = Modifier.padding(bottom = 8.dp)
         )
 
         NexoChatBubble(
             messageContent = message.content,
             sender = message.sender.username,
+            color = color,
             cornerCurvePosition = CornerCurvePosition.LEFT,
             formattedDateTime = message.formattedSentTime.asString()
         )
@@ -55,8 +59,9 @@ private fun OtherUserMessageUiPreview() {
                     id = "1",
                     username = "Alejo",
                     initials = "AL"
-                )
-            )
+                ),
+            ),
+            color = getChatBubbleColorForUser("1")
         )
     }
 }
