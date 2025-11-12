27a9017ddfb747f95e0ddbf3bc5c04c23468076c
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-12 13:40:45 -0500

    feat: Add `MessageList` composable

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/components/ChatDetailHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/components/ChatDetailHeader.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
@@ -39,7 +40,7 @@ import nexo.core.designsystem.generated.resources.Res as DesignSystemRes
 
 @Composable
 fun ChatDetailHeader(
-    chatUi: ChatUi,
+    chatUi: ChatUi?,
     isDetailPresent: Boolean,
     isChatOptionsDropDownOpen: Boolean,
     onChatOptionsClick: () -> Unit,
@@ -70,13 +71,17 @@ fun ChatDetailHeader(
             }
         }
 
-        ChatItemHeaderRow(
-            chat = chatUi,
-            modifier = Modifier.weight(1f)
-                .clickable {
-                    onManageChatClick()
-                }
-        )
+        if (chatUi != null) {
+            ChatItemHeaderRow(
+                chat = chatUi,
+                modifier = Modifier.weight(1f)
+                    .clickable {
+                        onManageChatClick()
+                    }
+            )
+        } else {
+            Spacer(Modifier.weight(1f))
+        }
 
         Box {
             NexoIconButton(
