94a9878f1be7d5a8c0bdf5e552f128baf34d67f7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-13 14:51:13 -0500

    feat: Add database factory and platform module to chat data module

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
@@ -1,14 +1,26 @@
 package dev.alejo.chat.data.di
 
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import dev.alejo.chat.data.chat.KtorChatParticipantService
 import dev.alejo.chat.data.chat.KtorChatService
+import dev.alejo.chat.database.DatabaseFactory
 import dev.alejo.chat.domain.chat.ChatParticipantService
 import dev.alejo.chat.domain.chat.ChatService
+import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
+expect val platformChatDataModule: Module
+
 val chatDataModule = module {
+    includes(platformChatDataModule)
+    single {
+        get<DatabaseFactory>()
+            .create()
+            .setDriver(BundledSQLiteDriver())
+            .build()
+    }
     singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
     singleOf(::KtorChatService) bind ChatService::class
 }
