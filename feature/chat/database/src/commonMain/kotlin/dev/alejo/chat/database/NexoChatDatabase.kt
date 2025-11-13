13c2c01bb72672e395586126870acc1a6261fea3
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-13 14:53:06 -0500

    feat: Add `NexoChatDatabase` Room database definition

---
+++ feature/chat/database/src/commonMain/kotlin/dev/alejo/chat/database/NexoChatDatabase.kt
@@ -0,0 +1,36 @@
+package dev.alejo.chat.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import dev.alejo.chat.database.dao.ChatDao
+import dev.alejo.chat.database.dao.ChatMessageDao
+import dev.alejo.chat.database.dao.ChatParticipantDao
+import dev.alejo.chat.database.dao.ChatParticipantsCrossRefDao
+import dev.alejo.chat.database.entities.ChatEntity
+import dev.alejo.chat.database.entities.ChatMessageEntity
+import dev.alejo.chat.database.entities.ChatParticipantCrossRef
+import dev.alejo.chat.database.entities.ChatParticipantEntity
+import dev.alejo.chat.database.view.LastMessageView
+
+@Database(
+    entities = [
+        ChatEntity::class,
+        ChatParticipantEntity::class,
+        ChatMessageEntity::class,
+        ChatParticipantCrossRef::class
+    ],
+    views = [
+        LastMessageView::class
+    ],
+    version = 1
+)
+abstract class NexoChatDatabase : RoomDatabase() {
+    abstract val chatDao: ChatDao
+    abstract val participantDao: ChatParticipantDao
+    abstract val chatMessageDao: ChatMessageDao
+    abstract val chatParticipantsCrossRefDao: ChatParticipantsCrossRefDao
+
+    companion object {
+        const val DB_NAME = "nexo.db"
+    }
+}
