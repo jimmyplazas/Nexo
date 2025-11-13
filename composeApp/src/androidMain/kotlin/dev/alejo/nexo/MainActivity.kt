bc5754ddc1f8d97e9b626047d526edb66298cf95
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-13 14:51:52 -0500

    fix: Remove MainActivity top spacing

--- composeApp/src/androidMain/kotlin/dev/alejo/nexo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/dev/alejo/nexo/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         var shouldShowSplashScreen = true
 
