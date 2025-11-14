284796447ffd67034459cdffd1e0d25613d62698
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-14 15:32:31 -0500

    fix: Remove redundant constructor in ViewModel

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_list_detail/ChatListDetailViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_list_detail/ChatListDetailViewModel.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class ChatListDetailViewModel : ViewModel() {
+class ChatListDetailViewModel() : ViewModel() {
 
     private val _state = MutableStateFlow(ChatListDetailState())
     val state = _state.asStateFlow()
