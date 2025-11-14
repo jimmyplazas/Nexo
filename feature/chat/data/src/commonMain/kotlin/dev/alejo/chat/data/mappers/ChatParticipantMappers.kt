30b2d2a1e4a9c970ec5098adb58d0d83a4069b66
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-14 15:33:30 -0500

    feat(chat): Add mappers for ChatParticipant entity

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/mappers/ChatParticipantMappers.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/mappers/ChatParticipantMappers.kt
@@ -1,10 +1,27 @@
 package dev.alejo.chat.data.mappers
 
 import dev.alejo.chat.data.dto.ChatParticipantDto
+import dev.alejo.chat.database.entities.ChatParticipantEntity
 import dev.alejo.chat.domain.models.ChatParticipant
 
 fun ChatParticipantDto.toDomain(): ChatParticipant {
     return ChatParticipant(
+        userId = userId,
+        username = username,
+        profilePictureUrl = profilePictureUrl
+    )
+}
+
+fun ChatParticipantEntity.toDomain(): ChatParticipant {
+    return ChatParticipant(
+        userId = userId,
+        username = username,
+        profilePictureUrl = profilePictureUrl
+    )
+}
+
+fun ChatParticipant.toEntity(): ChatParticipantEntity {
+    return ChatParticipantEntity(
         userId = userId,
         username = username,
         profilePictureUrl = profilePictureUrl
