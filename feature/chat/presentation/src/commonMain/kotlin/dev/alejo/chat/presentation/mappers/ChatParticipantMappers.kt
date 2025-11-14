cd3241cf3518bbea3dab634f6fe34d9166e2097b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-14 15:33:36 -0500

    feat: Add `User` to `ChatParticipantUi` mapper

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/mappers/ChatParticipantMappers.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/mappers/ChatParticipantMappers.kt
@@ -2,6 +2,7 @@ package dev.alejo.chat.presentation.mappers
 
 import dev.alejo.chat.domain.models.ChatParticipant
 import dev.alejo.core.designsystem.components.avatar.ChatParticipantUi
+import dev.alejo.core.domain.auth.User
 
 fun ChatParticipant.toUi(): ChatParticipantUi {
     return ChatParticipantUi(
@@ -9,5 +10,14 @@ fun ChatParticipant.toUi(): ChatParticipantUi {
         username = username,
         initials = initials,
         imageUrl = profilePictureUrl
+    )
+}
+
+fun User.toUi(): ChatParticipantUi {
+    return ChatParticipantUi(
+        id = id,
+        username = username,
+        initials = username.take(2).uppercase(),
+        imageUrl = profilePicture
     )
 }
