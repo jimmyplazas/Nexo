71fc4971d285ea16723dbbb222c2f11f54f7d86e
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-14 15:31:41 -0500

    feat: Implement `OfflineFirstChatRepository`

---
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/chat/OfflineFirstChatRepository.kt
@@ -0,0 +1,51 @@
+package dev.alejo.chat.data.chat
+
+import dev.alejo.chat.data.mappers.toDomain
+import dev.alejo.chat.data.mappers.toEntity
+import dev.alejo.chat.data.mappers.toLastMessageView
+import dev.alejo.chat.database.NexoChatDatabase
+import dev.alejo.chat.database.entities.ChatWithParticipants
+import dev.alejo.chat.domain.chat.ChatRepository
+import dev.alejo.chat.domain.chat.ChatService
+import dev.alejo.chat.domain.models.Chat
+import dev.alejo.core.domain.Result
+import dev.alejo.core.domain.onFailure
+import dev.alejo.core.domain.onSuccess
+import dev.alejo.core.domain.util.DataError
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class OfflineFirstChatRepository(
+    private val chatService: ChatService,
+    private val db: NexoChatDatabase
+) : ChatRepository {
+
+    override fun getChats(): Flow<List<Chat>> {
+        return db.chatDao.getChatsWithActiveParticipants()
+            .map { chatWithParticipants ->
+                chatWithParticipants.map { it.toDomain() }
+            }
+    }
+
+    override suspend fun fetchChats(): Result<List<Chat>, DataError.Remote> {
+        return chatService.getChats()
+            .onSuccess { chats ->
+                val chatWithParticipants = chats.map { chat ->
+                    ChatWithParticipants(
+                        chat = chat.toEntity(),
+                        participants = chat.participants.map { it.toEntity() },
+                        lastMessage = chat.lastMessage?.toLastMessageView()
+                    )
+                }
+                db.chatDao.upsertChatsWithParticipantsAndCrossRefs(
+                    chats = chatWithParticipants,
+                    participantDao = db.participantDao,
+                    crossRefDao = db.chatParticipantsCrossRefDao,
+                    messageDao = db.chatMessageDao
+                )
+            }
+            .onFailure { error ->
+
+            }
+    }
+}
