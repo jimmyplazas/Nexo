aeecb87c9befcde90ae9c198fe7ffad465f688c0
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-18 10:04:54 -0500

    feat: Add chat info and fetch by ID to repository

--- feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/chat/ChatRepository.kt
+++ feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/chat/ChatRepository.kt
@@ -1,11 +1,15 @@
 package dev.alejo.chat.domain.chat
 
 import dev.alejo.chat.domain.models.Chat
+import dev.alejo.chat.domain.models.ChatInfo
+import dev.alejo.core.domain.EmptyResult
 import dev.alejo.core.domain.Result
 import dev.alejo.core.domain.util.DataError
 import kotlinx.coroutines.flow.Flow
 
 interface ChatRepository {
     fun getChats(): Flow<List<Chat>>
+    fun getChatInfoById(chatId: String): Flow<ChatInfo>
     suspend fun fetchChats(): Result<List<Chat>, DataError.Remote>
+    suspend fun fetchChatById(chatId: String): EmptyResult<DataError.Remote>
 }
