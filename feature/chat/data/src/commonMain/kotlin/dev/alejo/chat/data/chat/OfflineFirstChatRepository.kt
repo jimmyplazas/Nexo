2b8f221e132caae3c513dcdb87965493eecef984
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-18 10:05:23 -0500

    feat: Implement chat fetching by ID

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/chat/OfflineFirstChatRepository.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/chat/OfflineFirstChatRepository.kt
@@ -8,11 +8,15 @@ import dev.alejo.chat.database.entities.ChatWithParticipants
 import dev.alejo.chat.domain.chat.ChatRepository
 import dev.alejo.chat.domain.chat.ChatService
 import dev.alejo.chat.domain.models.Chat
+import dev.alejo.chat.domain.models.ChatInfo
+import dev.alejo.core.domain.EmptyResult
 import dev.alejo.core.domain.Result
+import dev.alejo.core.domain.asEmptyResult
 import dev.alejo.core.domain.onFailure
 import dev.alejo.core.domain.onSuccess
 import dev.alejo.core.domain.util.DataError
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 
 class OfflineFirstChatRepository(
@@ -27,6 +31,12 @@ class OfflineFirstChatRepository(
             }
     }
 
+    override fun getChatInfoById(chatId: String): Flow<ChatInfo> {
+        return db.chatDao.getChatInfoById(chatId)
+            .filterNotNull()
+            .map { it.toDomain() }
+    }
+
     override suspend fun fetchChats(): Result<List<Chat>, DataError.Remote> {
         return chatService.getChats()
             .onSuccess { chats ->
@@ -39,7 +49,7 @@ class OfflineFirstChatRepository(
                 }
                 db.chatDao.upsertChatsWithParticipantsAndCrossRefs(
                     chats = chatWithParticipants,
-                    participantDao = db.participantDao,
+                    participantDao = db.chatParticipantDao,
                     crossRefDao = db.chatParticipantsCrossRefDao,
                     messageDao = db.chatMessageDao
                 )
@@ -47,5 +57,19 @@ class OfflineFirstChatRepository(
             .onFailure { error ->
 
             }
+    }
+
+    override suspend fun fetchChatById(chatId: String): EmptyResult<DataError.Remote> {
+        return chatService
+            .getChatById(chatId)
+            .onSuccess { chat ->
+                db.chatDao.upsertChatWithParticipantsAndCrossRefs(
+                    chat = chat.toEntity(),
+                    participants = chat.participants.map { it.toEntity() },
+                    participantDao = db.chatParticipantDao,
+                    crossRefDao = db.chatParticipantsCrossRefDao
+                )
+            }
+            .asEmptyResult()
     }
 }
