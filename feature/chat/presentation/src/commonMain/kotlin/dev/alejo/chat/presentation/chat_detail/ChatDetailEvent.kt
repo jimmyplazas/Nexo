d30c8ef60e4c4705a500f747c89c6280f1741718
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-24 16:48:54 -0500

    feat: Add `ChatDetailEvent` sealed interface

---
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/ChatDetailEvent.kt
@@ -0,0 +1,8 @@
+package dev.alejo.chat.presentation.chat_detail
+
+import dev.alejo.core.presentation.util.UiText
+
+sealed interface ChatDetailEvent {
+    data object OnChatLeft : ChatDetailEvent
+    data class OnError(val error: UiText) : ChatDetailEvent
+}
