d0b85f2131f3722ff65b21f58177a5b547898b89
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-24 19:41:49 -0500

    feat: Refactor `ManageChatAction` for chat participants

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/components/manage_chat/ManageChatAction.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/components/manage_chat/ManageChatAction.kt
@@ -3,5 +3,9 @@ package dev.alejo.chat.presentation.components.manage_chat
 sealed interface ManageChatAction {
     data object OnAddClick : ManageChatAction
     data object OnDismissDialog : ManageChatAction
-    data object OnCreateChatClick : ManageChatAction
+    data object OnPrimaryActionClick : ManageChatAction
+
+    sealed interface ChatParticipants : ManageChatAction {
+        data class OnSelectChat(val chatId: String?) : ManageChatAction
+    }
 }
