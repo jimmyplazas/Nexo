e352a8d34db2161b3a391bcc442a1065d167f1ef
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-25 15:32:23 -0500

    feat(chat): Add `AppLifecycleObserver` to iOS Koin module

--- feature/chat/data/src/iosMain/kotlin/dev/alejo/chat/data/di/chatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/dev/alejo/chat/data/di/chatDataModule.ios.kt
@@ -1,8 +1,11 @@
 package dev.alejo.chat.data.di
 
+import dev.alejo.chat.data.lifecycle.AppLifecycleObserver
 import dev.alejo.chat.database.DatabaseFactory
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val platformChatDataModule = module {
     single { DatabaseFactory() }
+    singleOf(::AppLifecycleObserver)
 }
