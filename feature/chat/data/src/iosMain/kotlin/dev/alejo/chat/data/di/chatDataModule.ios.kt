def7e51fd6e6d0496e3d008a63c9b4ff3c5ffd9a
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-25 16:18:37 -0500

    feat: Add `ConnectivityObserver` to iOS Koin module

--- feature/chat/data/src/iosMain/kotlin/dev/alejo/chat/data/di/chatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/dev/alejo/chat/data/di/chatDataModule.ios.kt
@@ -1,6 +1,7 @@
 package dev.alejo.chat.data.di
 
 import dev.alejo.chat.data.lifecycle.AppLifecycleObserver
+import dev.alejo.chat.data.network.ConnectivityObserver
 import dev.alejo.chat.database.DatabaseFactory
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 actual val platformChatDataModule = module {
     single { DatabaseFactory() }
     singleOf(::AppLifecycleObserver)
+    singleOf(::ConnectivityObserver)
 }
