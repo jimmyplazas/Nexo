a8e303ae39e6757944ec888269a79f26c45679dc
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-24 19:41:15 -0500

    feat: Add participant management to chat repository

--- feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/chat/ChatRepository.kt
+++ feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/chat/ChatRepository.kt
@@ -2,6 +2,7 @@ package dev.alejo.chat.domain.chat
 
 import dev.alejo.chat.domain.models.Chat
 import dev.alejo.chat.domain.models.ChatInfo
+import dev.alejo.chat.domain.models.ChatParticipant
 import dev.alejo.core.domain.EmptyResult
 import dev.alejo.core.domain.Result
 import dev.alejo.core.domain.util.DataError
@@ -10,8 +11,13 @@ import kotlinx.coroutines.flow.Flow
 interface ChatRepository {
     fun getChats(): Flow<List<Chat>>
     fun getChatInfoById(chatId: String): Flow<ChatInfo>
+    fun getActiveParticipantsByChatId(chatId: String): Flow<List<ChatParticipant>>
     suspend fun fetchChats(): Result<List<Chat>, DataError.Remote>
     suspend fun fetchChatById(chatId: String): EmptyResult<DataError.Remote>
     suspend fun createChat(otherUserIds: List<String>): Result<Chat, DataError.Remote>
     suspend fun leaveChat(chatId: String): EmptyResult<DataError.Remote>
+    suspend fun addParticipantsToChat(
+        chatId: String,
+        userIds: List<String>
+    ): Result<Chat, DataError.Remote>
 }
