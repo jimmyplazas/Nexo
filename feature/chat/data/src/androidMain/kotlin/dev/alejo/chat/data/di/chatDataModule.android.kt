b89dc4c011446d06bb7a58385ac14e95a48be7bf
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-25 15:32:27 -0500

    feat: Add `AppLifecycleObserver` to chat data module

--- feature/chat/data/src/androidMain/kotlin/dev/alejo/chat/data/di/chatDataModule.android.kt
+++ feature/chat/data/src/androidMain/kotlin/dev/alejo/chat/data/di/chatDataModule.android.kt
@@ -1,9 +1,12 @@
 package dev.alejo.chat.data.di
 
+import dev.alejo.chat.data.lifecycle.AppLifecycleObserver
 import dev.alejo.chat.database.DatabaseFactory
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val platformChatDataModule = module {
     single { DatabaseFactory(androidContext()) }
+    singleOf(::AppLifecycleObserver)
 }
