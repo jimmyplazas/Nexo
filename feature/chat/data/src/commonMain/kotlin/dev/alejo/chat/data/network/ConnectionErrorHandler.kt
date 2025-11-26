8eb459d2190203e17184766df90b5bdb13593141
Jimmyale3102 <jimmyale3102@gmail.com>
2025-11-26 10:48:47 -0500

    feat: Add expect class for ConnectionErrorHandler

---
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/network/ConnectionErrorHandler.kt
@@ -0,0 +1,9 @@
+package dev.alejo.chat.data.network
+
+import dev.alejo.chat.domain.models.ConnectionState
+
+expect class ConnectionErrorHandler {
+    fun getConnectionStateForError(cause: Throwable): ConnectionState
+    fun transformException(exception: Throwable): Throwable
+    fun isRetriableError(cause: Throwable): Boolean
+}
