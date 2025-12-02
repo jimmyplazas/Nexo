cabc5dfa52edbad7e8d10ddf04d1669f72194063
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-01 21:00:04 -0500

    feat: Add websocket URL constant

--- core/data/src/commonMain/kotlin/dev/alejo/core/data/networking/UrlConstants.kt
+++ core/data/src/commonMain/kotlin/dev/alejo/core/data/networking/UrlConstants.kt
@@ -2,4 +2,5 @@ package dev.alejo.core.data.networking
 
 object UrlConstants {
     const val BASE_URL_HTTP = "https://nexo.jimmyplazas.dev/api"
+    const val BASE_URL_WS = "wss://nexo.jimmyplazas.dev/ws"
 }
