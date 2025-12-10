9190794cf02c26a3ce9e1f78cb9b056d18723856
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 17:27:00 -0500

    feat: Add message and WebSocket dependencies

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
@@ -4,10 +4,16 @@ import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import dev.alejo.chat.data.chat.KtorChatParticipantService
 import dev.alejo.chat.data.chat.KtorChatService
 import dev.alejo.chat.data.chat.OfflineFirstChatRepository
+import dev.alejo.chat.data.chat.WebSocketChatConnectionClient
+import dev.alejo.chat.data.message.OfflineFirstMessageRepository
+import dev.alejo.chat.data.network.KtorWebSocketConnector
 import dev.alejo.chat.database.DatabaseFactory
+import dev.alejo.chat.domain.chat.ChatConnectionClient
 import dev.alejo.chat.domain.chat.ChatParticipantService
 import dev.alejo.chat.domain.chat.ChatRepository
 import dev.alejo.chat.domain.chat.ChatService
+import dev.alejo.chat.domain.message.MessageRepository
+import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -17,13 +23,21 @@ expect val platformChatDataModule: Module
 
 val chatDataModule = module {
     includes(platformChatDataModule)
+    singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
+    singleOf(::KtorChatService) bind ChatService::class
+    singleOf(:: OfflineFirstChatRepository) bind ChatRepository::class
+    singleOf(:: OfflineFirstMessageRepository) bind MessageRepository::class
+    singleOf(:: WebSocketChatConnectionClient) bind ChatConnectionClient::class
+    singleOf(:: KtorWebSocketConnector)
     single {
         get<DatabaseFactory>()
             .create()
             .setDriver(BundledSQLiteDriver())
             .build()
     }
-    singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
-    singleOf(::KtorChatService) bind ChatService::class
-    singleOf(:: OfflineFirstChatRepository) bind ChatRepository::class
+    single {
+        Json {
+            ignoreUnknownKeys = true
+        }
+    }
 }
