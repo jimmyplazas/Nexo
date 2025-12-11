490e3a187f478d029e9c03c22a9027b02044c88f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:10:44 -0500

    feat: Add message fetching to MessageRepository

--- feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/message/MessageRepository.kt
+++ feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/message/MessageRepository.kt
@@ -1,8 +1,12 @@
 package dev.alejo.chat.domain.message
 
+import dev.alejo.chat.domain.models.ChatMessage
 import dev.alejo.chat.domain.models.ChatMessageDeliveryStatus
+import dev.alejo.chat.domain.models.MessageWithSender
 import dev.alejo.core.domain.EmptyResult
+import dev.alejo.core.domain.Result
 import dev.alejo.core.domain.util.DataError
+import kotlinx.coroutines.flow.Flow
 
 interface MessageRepository {
 
@@ -10,4 +14,11 @@ interface MessageRepository {
         messageId: String,
         status: ChatMessageDeliveryStatus
     ) : EmptyResult<DataError.Local>
+
+    suspend fun fetchMessages(
+        chatId: String,
+        before: String? = null
+    ): Result<List<ChatMessage>, DataError>
+
+    fun getMessagesForChat(chatId: String): Flow<List<MessageWithSender>>
 }
