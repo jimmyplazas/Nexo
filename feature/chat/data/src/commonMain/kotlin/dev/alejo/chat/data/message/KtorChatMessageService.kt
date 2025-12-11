7aee125e12b21bee6feb0213dc4e5e6aed5a704b
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:10:23 -0500

    feat: Implement Ktor chat message service

---
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/message/KtorChatMessageService.kt
@@ -0,0 +1,31 @@
+package dev.alejo.chat.data.message
+
+import dev.alejo.chat.data.dto.ChatMessageDto
+import dev.alejo.chat.data.mappers.toDomain
+import dev.alejo.chat.domain.message.ChatMessageService
+import dev.alejo.chat.domain.models.ChatMessage
+import dev.alejo.core.data.networking.get
+import dev.alejo.core.domain.Result
+import dev.alejo.core.domain.map
+import dev.alejo.core.domain.util.DataError
+import io.ktor.client.HttpClient
+
+class KtorChatMessageService(
+    private val httpClient: HttpClient
+) : ChatMessageService {
+
+    override suspend fun fetchMessages(
+        chatId: String,
+        before: String?
+    ): Result<List<ChatMessage>, DataError.Remote> {
+        return httpClient.get<List<ChatMessageDto>>(
+            route = "/chat/$chatId/messages",
+            queryParams = buildMap {
+                this["pageSize"] = ChatMessageConstant.PAGE_SIZE
+                if (before != null) {
+                    this["before"] = before
+                }
+            }
+        ).map { it.map { it.toDomain() } }
+    }
+}
