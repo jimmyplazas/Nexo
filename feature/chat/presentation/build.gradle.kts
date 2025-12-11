a132bc1c616fe06ea145bdf5282fac5f4068debe
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:10:59 -0500

    feat: Add material3 adaptive and datetime dependencies

--- feature/chat/presentation/build.gradle.kts
+++ feature/chat/presentation/build.gradle.kts
@@ -17,8 +17,11 @@ kotlin {
                 implementation(projects.core.designsystem)
                 implementation(projects.feature.chat.domain)
 
+                implementation(libs.material3.adaptive)
+                implementation(libs.material3.adaptive.layout)
                 implementation(libs.material3.adaptive.navigation)
                 implementation(libs.jetbrains.compose.backhandler)
+                implementation(libs.kotlinx.datetime)
 
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
