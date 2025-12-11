45ce2e920a3f0a7bb53d20f9cf6009493c1beb61
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:11:08 -0500

    feat: Add `ConnectionErrorHandler` to DI

--- feature/chat/data/src/androidMain/kotlin/dev/alejo/chat/data/di/chatDataModule.android.kt
+++ feature/chat/data/src/androidMain/kotlin/dev/alejo/chat/data/di/chatDataModule.android.kt
@@ -1,6 +1,7 @@
 package dev.alejo.chat.data.di
 
 import dev.alejo.chat.data.lifecycle.AppLifecycleObserver
+import dev.alejo.chat.data.network.ConnectionErrorHandler
 import dev.alejo.chat.data.network.ConnectivityObserver
 import dev.alejo.chat.database.DatabaseFactory
 import org.koin.android.ext.koin.androidContext
@@ -11,4 +12,5 @@ actual val platformChatDataModule = module {
     single { DatabaseFactory(androidContext()) }
     singleOf(::AppLifecycleObserver)
     singleOf(::ConnectivityObserver)
+    singleOf(::ConnectionErrorHandler)
 }
