bf7306c57b6d13acb88b8e6c101695ff71c18ca5
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:09:52 -0500

    feat: Add chat message domain to presentation mapper

---
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/mappers/ChatMessageMappers.kt
@@ -0,0 +1,25 @@
+package dev.alejo.chat.presentation.mappers
+
+import dev.alejo.chat.domain.models.MessageWithSender
+import dev.alejo.chat.presentation.model.MessageUi
+import dev.alejo.chat.presentation.util.DateUtils
+
+fun MessageWithSender.toUi(localUserId: String): MessageUi {
+    val isFromCLocalUser = this.sender.userId == localUserId
+    return if (isFromCLocalUser) {
+        MessageUi.LocalUserMessage(
+            id = message.id,
+            content = message.content,
+            deliveryStatus,
+            isMenuOpen = false,
+            formattedSentTime = DateUtils.formatMessageTime(instant = message.createdAt)
+        )
+    } else {
+        MessageUi.OtherUserMessage(
+            id = message.id,
+            content = message.content,
+            formattedSentTime = DateUtils.formatMessageTime(instant = message.createdAt),
+            sender = this.sender.toUi()
+        )
+    }
+}
