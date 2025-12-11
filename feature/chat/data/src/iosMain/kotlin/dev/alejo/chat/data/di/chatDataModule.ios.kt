6e2720c5ed2ebca1aef742b1fc63d3ae6787bab5
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:12:25 -0500

    feat: Add `ConnectionErrorHandler` to iOS DI module

--- feature/chat/data/src/iosMain/kotlin/dev/alejo/chat/data/di/chatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/dev/alejo/chat/data/di/chatDataModule.ios.kt
@@ -1,6 +1,7 @@
 package dev.alejo.chat.data.di
 
 import dev.alejo.chat.data.lifecycle.AppLifecycleObserver
+import dev.alejo.chat.data.network.ConnectionErrorHandler
 import dev.alejo.chat.data.network.ConnectivityObserver
 import dev.alejo.chat.database.DatabaseFactory
 import org.koin.core.module.dsl.singleOf
@@ -10,4 +11,5 @@ actual val platformChatDataModule = module {
     single { DatabaseFactory() }
     singleOf(::AppLifecycleObserver)
     singleOf(::ConnectivityObserver)
+    singleOf(::ConnectionErrorHandler)
 }
