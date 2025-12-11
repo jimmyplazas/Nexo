cdd418b5b04247ffcb010d03007a84ecfdd63828
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:10:03 -0500

    feat: Add `ChatMessageService` interface

---
+++ feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/message/ChatMessageService.kt
@@ -0,0 +1,12 @@
+package dev.alejo.chat.domain.message
+
+import dev.alejo.chat.domain.models.ChatMessage
+import dev.alejo.core.domain.Result
+import dev.alejo.core.domain.util.DataError
+
+interface ChatMessageService {
+    suspend fun fetchMessages(
+        chatId: String,
+        before: String? = null
+    ): Result<List<ChatMessage>, DataError.Remote>
+}
