633d32a051819b58f589087805cd440b78c262ab
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:12:33 -0500

    feat: Add `ConnectionErrorHandler` to iOS DI module

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/di/chatDataModule.kt
@@ -5,13 +5,16 @@ import dev.alejo.chat.data.chat.KtorChatParticipantService
 import dev.alejo.chat.data.chat.KtorChatService
 import dev.alejo.chat.data.chat.OfflineFirstChatRepository
 import dev.alejo.chat.data.chat.WebSocketChatConnectionClient
+import dev.alejo.chat.data.message.KtorChatMessageService
 import dev.alejo.chat.data.message.OfflineFirstMessageRepository
+import dev.alejo.chat.data.network.ConnectionRetryHandler
 import dev.alejo.chat.data.network.KtorWebSocketConnector
 import dev.alejo.chat.database.DatabaseFactory
 import dev.alejo.chat.domain.chat.ChatConnectionClient
 import dev.alejo.chat.domain.chat.ChatParticipantService
 import dev.alejo.chat.domain.chat.ChatRepository
 import dev.alejo.chat.domain.chat.ChatService
+import dev.alejo.chat.domain.message.ChatMessageService
 import dev.alejo.chat.domain.message.MessageRepository
 import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
@@ -23,12 +26,16 @@ expect val platformChatDataModule: Module
 
 val chatDataModule = module {
     includes(platformChatDataModule)
+
     singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
     singleOf(::KtorChatService) bind ChatService::class
     singleOf(:: OfflineFirstChatRepository) bind ChatRepository::class
     singleOf(:: OfflineFirstMessageRepository) bind MessageRepository::class
     singleOf(:: WebSocketChatConnectionClient) bind ChatConnectionClient::class
+    singleOf(:: ConnectionRetryHandler)
     singleOf(:: KtorWebSocketConnector)
+    singleOf(:: KtorChatMessageService) bind ChatMessageService::class
+
     single {
         get<DatabaseFactory>()
             .create()
