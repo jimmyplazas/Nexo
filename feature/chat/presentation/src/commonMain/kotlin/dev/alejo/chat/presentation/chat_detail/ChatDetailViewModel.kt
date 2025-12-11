d578d2f8c65ec08b30a754ed70a94060f9fb4a65
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-10 20:13:17 -0500

    feat: Observe messages and connection state in ChatDetail

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/ChatDetailViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_detail/ChatDetailViewModel.kt
@@ -5,7 +5,10 @@ package dev.alejo.chat.presentation.chat_detail
 import androidx.compose.foundation.text.input.clearText
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.alejo.chat.domain.chat.ChatConnectionClient
 import dev.alejo.chat.domain.chat.ChatRepository
+import dev.alejo.chat.domain.message.MessageRepository
+import dev.alejo.chat.domain.models.ConnectionState
 import dev.alejo.chat.presentation.mappers.toUi
 import dev.alejo.core.domain.auth.SessionStorage
 import dev.alejo.core.domain.onFailure
@@ -16,8 +19,12 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
@@ -26,7 +33,9 @@ import kotlinx.coroutines.launch
 
 class ChatDetailViewModel(
     private val chatRepository: ChatRepository,
-    private val sessionStorage: SessionStorage
+    private val messageRepository: MessageRepository,
+    private val sessionStorage: SessionStorage,
+    private val connectionClient: ChatConnectionClient
 ) : ViewModel() {
 
     private val _events = Channel<ChatDetailEvent>()
@@ -66,7 +75,8 @@ class ChatDetailViewModel(
         }
         .onStart {
             if (!hasLoadedInitialData) {
-
+                observeConnectionState()
+                observeChatMessages()
                 hasLoadedInitialData = true
             }
         }
@@ -100,6 +110,64 @@ class ChatDetailViewModel(
                 chatRepository.fetchChatById(chatId)
             }
         }
+    }
+
+    private fun observeChatMessages() {
+        val currentMessages = state
+            .map { it.messages }
+            .distinctUntilChanged()
+
+        val newMessages = _chatId.flatMapLatest { chatId ->
+                if(chatId != null) {
+                    messageRepository.getMessagesForChat(chatId)
+                } else emptyFlow()
+            }
+            .combine(sessionStorage.observeAuthInf()) { messages, authInfo ->
+                if (authInfo == null) {
+                    return@combine messages
+                }
+
+                _state.update {
+                    it.copy(
+                        messages = messages.map { it.toUi(authInfo.user.id)  }
+                    )
+                }
+                messages
+            }
+
+        val isNearBottom = state.map { it.isNearBottom }.distinctUntilChanged()
+
+        combine(
+            currentMessages,
+            newMessages,
+            isNearBottom
+        ) { currentMessages, newMessages, isNearBottom ->
+            val lastNewId = newMessages.lastOrNull()?.message?.id
+            val lastCurrentId = currentMessages.lastOrNull()?.id
+
+            if (lastNewId != lastCurrentId && isNearBottom) {
+                _events.send(ChatDetailEvent.OnNewMessage)
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun observeConnectionState() {
+        connectionClient
+            .connectionState
+            .onEach { connectionState ->
+                if (connectionState == ConnectionState.CONNECTED) {
+                    _chatId.value?.let {
+                        messageRepository.fetchMessages(it, before = null)
+                    }
+                }
+
+                _state.update {
+                    it.copy(
+                        connectionState = connectionState
+                    )
+                }
+            }
+            .launchIn(viewModelScope)
     }
 
     private fun onLeaveChatClick() {
