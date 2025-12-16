0b4d60ea641c154ae11a99f22fe9a0b8112afa3d
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-16 12:19:37 -0500

    feat: Add `Paginator` class for handling paginated data

---
+++ core/domain/src/commonMain/kotlin/dev/alejo/core/domain/util/Paginator.kt
@@ -0,0 +1,58 @@
+package dev.alejo.core.domain.util
+
+import dev.alejo.core.domain.Result
+import dev.alejo.core.domain.onFailure
+import dev.alejo.core.domain.onSuccess
+import kotlinx.coroutines.ensureActive
+import kotlin.coroutines.coroutineContext
+
+class Paginator<Key, Item>(
+    private val initialKey: Key,
+    private val onLoadUpdated: (Boolean) -> Unit,
+    private val onRequest: suspend (nextKey: Key) -> Result<List<Item>, DataError>,
+    private val getNextKey: suspend(List<Item>) -> Key,
+    private val onError: suspend (Throwable?) -> Unit,
+    private val onSuccess: suspend (items: List<Item>, newKey: Key) -> Unit
+) {
+    private var currentKey = initialKey
+    private var isMakingRequest = false
+    private var lastRequestKey: Key? = null
+
+    suspend fun loadNextItems() {
+        if (isMakingRequest) {
+            return
+        }
+
+        if (currentKey != null && currentKey == lastRequestKey) {
+            return
+        }
+
+        isMakingRequest = true
+        lastRequestKey = currentKey
+        onLoadUpdated(true)
+
+        try {
+            onRequest(currentKey)
+                .onSuccess { items ->
+                    val newKey = getNextKey(items)
+                    onSuccess(items, newKey)
+                    currentKey = newKey
+                }
+                .onFailure { error ->
+                    onError(DataErrorException(error))
+                }
+        } catch (e: Exception) {
+            coroutineContext.ensureActive()
+
+            onError(e)
+        } finally {
+            onLoadUpdated(false)
+            isMakingRequest = false
+        }
+    }
+
+    fun reset() {
+        currentKey = initialKey
+        lastRequestKey = null
+    }
+}
