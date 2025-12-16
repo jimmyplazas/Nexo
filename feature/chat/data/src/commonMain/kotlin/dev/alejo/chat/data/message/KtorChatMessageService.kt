1e941ce53463b183cfe532d1cc79f58126384863
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-16 11:42:06 -0500

    feat: Implement delete message functionality

--- feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/message/KtorChatMessageService.kt
+++ feature/chat/data/src/commonMain/kotlin/dev/alejo/chat/data/message/KtorChatMessageService.kt
@@ -4,7 +4,9 @@ import dev.alejo.chat.data.dto.ChatMessageDto
 import dev.alejo.chat.data.mappers.toDomain
 import dev.alejo.chat.domain.message.ChatMessageService
 import dev.alejo.chat.domain.models.ChatMessage
+import dev.alejo.core.data.networking.delete
 import dev.alejo.core.data.networking.get
+import dev.alejo.core.domain.EmptyResult
 import dev.alejo.core.domain.Result
 import dev.alejo.core.domain.map
 import dev.alejo.core.domain.util.DataError
@@ -27,5 +29,13 @@ class KtorChatMessageService(
                 }
             }
         ).map { it.map { it.toDomain() } }
+    }
+
+    override suspend fun deleteMessage(
+        messageId: String
+    ): EmptyResult<DataError.Remote> {
+        return httpClient.delete(
+            route = "/messages/$messageId"
+        )
     }
 }
