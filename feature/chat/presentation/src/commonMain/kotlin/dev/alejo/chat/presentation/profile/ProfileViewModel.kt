9a0604269e5638c6abc0aaf33d55b1d396b449e7
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-18 16:32:41 -0500

    feat: Implement change password logic in `ProfileViewModel`

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/profile/ProfileViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/profile/ProfileViewModel.kt
@@ -1,13 +1,33 @@
 package dev.alejo.chat.presentation.profile
 
+import androidx.compose.foundation.text.input.clearText
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.alejo.core.domain.auth.AuthService
+import dev.alejo.core.domain.onFailure
+import dev.alejo.core.domain.onSuccess
+import dev.alejo.core.domain.util.DataError
+import dev.alejo.core.domain.validation.PasswordValidator
+import dev.alejo.core.presentation.util.UiText
+import dev.alejo.core.presentation.util.toUiText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import nexo.feature.chat.presentation.generated.resources.Res
+import nexo.feature.chat.presentation.generated.resources.error_current_password_equals_to_new_password
+import nexo.feature.chat.presentation.generated.resources.error_current_password_incorrect
 
-class ProfileViewModel : ViewModel() {
+class ProfileViewModel(
+    private val authService: AuthService
+) : ViewModel() {
 
     private var hasLoadedInitialData = false
 
@@ -15,7 +35,7 @@ class ProfileViewModel : ViewModel() {
     val state = _state
         .onStart {
             if (!hasLoadedInitialData) {
-                /** Load initial data here **/
+                observeCanChangePassword()
                 hasLoadedInitialData = true
             }
         }
@@ -27,7 +47,102 @@ class ProfileViewModel : ViewModel() {
 
     fun onAction(action: ProfileAction) {
         when (action) {
+            is ProfileAction.OnChangePasswordClick -> changePassword()
+            is ProfileAction.OnToggleCurrentPasswordVisibility -> toggleCurrentPasswordVisibility()
+            is ProfileAction.OnToggleNewPasswordVisibility -> toggleNewPasswordVisibility()
             else -> Unit
+        }
+    }
+
+    private fun toggleNewPasswordVisibility() {
+        _state.update {
+            it.copy(
+                isNewPasswordVisible = !it.isNewPasswordVisible
+            )
+        }
+    }
+
+    private fun toggleCurrentPasswordVisibility() {
+        _state.update {
+            it.copy(
+                isCurrentPasswordVisible = !it.isCurrentPasswordVisible
+            )
+        }
+    }
+
+    private fun observeCanChangePassword() {
+        val currentPasswordFlow = snapshotFlow {
+            state.value.currentPasswordTextState.text.toString()
+        }.map { it.isNotBlank() }.distinctUntilChanged()
+        val newPasswordFlow = snapshotFlow {
+            state.value.newPasswordTextState.text.toString()
+        }.map {
+            PasswordValidator.validate(it).isValidPassword
+        }.distinctUntilChanged()
+
+        combine(
+            currentPasswordFlow,
+            newPasswordFlow
+        ) { currentPasswordValid, newPasswordValid ->
+            _state.update {
+                it.copy(
+                    canChangePassword = currentPasswordValid && newPasswordValid
+                )
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun changePassword() {
+        if (!state.value.canChangePassword && state.value.isChangingPassword) {
+            return
+        }
+
+        _state.update {
+            it.copy(
+                isChangingPassword = true,
+                isPasswordChangeSuccessful = false
+            )
+        }
+
+        viewModelScope.launch {
+            val currentPassword = state.value.currentPasswordTextState.text.toString()
+            val newPassword = state.value.newPasswordTextState.text.toString()
+            authService
+                .changePassword(
+                    currentPassword = currentPassword,
+                    newPassword = newPassword
+                )
+                .onSuccess {
+                    state.value.currentPasswordTextState.clearText()
+                    state.value.newPasswordTextState.clearText()
+
+                    _state.update {
+                        it.copy(
+                            isChangingPassword = false,
+                            newPasswordError = null,
+                            isCurrentPasswordVisible = false,
+                            isNewPasswordVisible = false,
+                            isPasswordChangeSuccessful = true
+                        )
+                    }
+                }
+                .onFailure { error ->
+                    val errorMessage = when (error) {
+                        DataError.Remote.UNAUTHORIZED -> {
+                            UiText.Resource(Res.string.error_current_password_incorrect)
+                        }
+                        DataError.Remote.CONFLICT -> {
+                            UiText.Resource(Res.string.error_current_password_equals_to_new_password)
+                        }
+                        else -> error.toUiText()
+                    }
+                    _state.update {
+                        it.copy(
+                            isChangingPassword = false,
+                            newPasswordError = errorMessage
+                        )
+                    }
+                }
         }
     }
 
