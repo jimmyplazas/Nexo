718e70968b76200c6846e0fc5781d2b0e520263f
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-18 16:32:59 -0500

    feat: Add `changePassword` to AuthService

--- core/domain/src/commonMain/kotlin/dev/alejo/core/domain/auth/AuthService.kt
+++ core/domain/src/commonMain/kotlin/dev/alejo/core/domain/auth/AuthService.kt
@@ -18,4 +18,8 @@ interface AuthService {
     suspend fun verifyEmail(token: String): EmptyResult<DataError.Remote>
     suspend fun forgotPassword(email: String): EmptyResult<DataError.Remote>
     suspend fun resetPassword(token: String, newPassword: String): EmptyResult<DataError.Remote>
+    suspend fun changePassword(
+        currentPassword: String,
+        newPassword: String
+    ) : EmptyResult<DataError.Remote>
 }
