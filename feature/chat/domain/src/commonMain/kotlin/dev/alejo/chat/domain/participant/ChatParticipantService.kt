b860a3cdc2b4f9e2da4ae0c9aa1fa5db6123df8a
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-19 16:17:51 -0500

    feat: Add profile picture upload service methods

--- feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/participant/ChatParticipantService.kt
+++ feature/chat/domain/src/commonMain/kotlin/dev/alejo/chat/domain/participant/ChatParticipantService.kt
@@ -1,6 +1,8 @@
 package dev.alejo.chat.domain.participant
 
 import dev.alejo.chat.domain.models.ChatParticipant
+import dev.alejo.chat.domain.models.ProfilePictureUploadUrls
+import dev.alejo.core.domain.EmptyResult
 import dev.alejo.core.domain.Result
 import dev.alejo.core.domain.util.DataError
 
@@ -8,5 +10,21 @@ interface ChatParticipantService {
     suspend fun searchParticipant(
         query: String
     ): Result<ChatParticipant, DataError.Remote>
+
     suspend fun getLocalParticipant(): Result<ChatParticipant, DataError.Remote>
+
+    suspend fun getProfilePictureUploadUrl(
+        mimeType: String
+    ): Result<ProfilePictureUploadUrls, DataError.Remote>
+
+    suspend fun uploadProfilePicture(
+        uploadUrl: String,
+        imageBytes: ByteArray,
+        headers: Map<String, String>
+    ): EmptyResult<DataError.Remote>
+
+    suspend fun confirmProfilePictureUpload(
+        publicUrl: String
+    ): EmptyResult<DataError.Remote>
+
 }
