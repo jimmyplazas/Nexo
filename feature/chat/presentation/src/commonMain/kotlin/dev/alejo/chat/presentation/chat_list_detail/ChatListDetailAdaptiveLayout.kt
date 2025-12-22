cb480b0694719a0334cbe28ac9eaa3cdd1ce2475
Jimmyale3102 <jimmyale3102@gmail.com>
2025-12-22 17:15:31 -0500

    refactor: Rename `onConfirmLogoutCLick` to `onSuccessfulLogout`

--- feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
+++ feature/chat/presentation/src/commonMain/kotlin/dev/alejo/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
@@ -82,7 +82,7 @@ fun ChatListDetailAdaptiveLayout(
                             )
                         }
                     },
-                    onConfirmLogoutCLick = onLogout,
+                    onSuccessfulLogout = onLogout,
                     onCreateChatCLick = {
                         chatListDetailViewModel.onAction(ChatListDetailAction.OnCreateChatClick)
                     },
